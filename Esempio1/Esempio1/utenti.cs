02de0837e2b2c5c8b0ef538619aec6e88fe183fa
Andrea Colleoni <andrea@colleoni.info>
2020-02-19 11:35:21 +0100

    test branch

--- Esempio1/Esempio1/utenti.cs
+++ Esempio1/Esempio1/utenti.cs
@@ -1,11 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Esempio1
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     [Table("utenti")]
     public partial class utenti
     {
