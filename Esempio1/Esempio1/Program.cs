ea22dd979fbb0b5f8efe3f555bc0e8f5382b7445
Andrea Colleoni <andrea@colleoni.info>
2020-02-19 11:38:37 +0100

    update

--- Esempio1/Esempio1/Program.cs
+++ Esempio1/Esempio1/Program.cs
@@ -14,6 +14,7 @@ namespace Esempio1
         [STAThread]
         static void Main()
         {
+            // commento
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
